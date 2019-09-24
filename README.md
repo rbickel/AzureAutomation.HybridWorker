dec610ae2e294d0a9a7b250744075f44349360eb
Raphael Bickel <rabickel@microsoft.com>
2019-09-24 11:04:59 +0200

    Add script and readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # AzureAutomation.HybridWorker
 Onboarding script for unattended hybrid worker installation
+
+## Powershell script to onboard new machines as Hybrid Worker for Azure Automation
+> Note: The Azure Automation Account and the Azure Log Analytics workspace must be in the same region
+
+```powershell
+#Install a new hybrid worker on a VM and register it to the target Azure Automazion Account
+$hybridWorkerGroupName = "myGroup"
+$workspaceId = "62b7bb25-7bfa-4006-bffc-a1c8667e37f6"
+$workspaceKey = "HxxiiaHH292+np3rrG03qIFGoqiJL1pdn/fHagV+VwFsqfQxstL5Pt+RuVM1goWyA9CbxZMiJChhF6IgKaphGQ=="
+$automationAccountEndpoint = "https://we-agentservice-prod-1.azure-automation.net/accounts/121d7ffa-30e3-41df-a028-d96a1c4f3f63"
+$automationAccountKey = "+OAZg2eFUSWPDssfKSQ/9pNoodShYP0yh3MQG7m83zbGUG5VjW5pjIfZBndYNfBNLsndiHYQLlyjkrmhgyE8eg="
+
+./hybridworker.ps1 -HybridGroupName $hybridWorkerGroupName -WorkspaceId $workspaceId -WorkspaceKey $workspaceKey -AutomationAccountEndpoint $automationAccountKey -AutomationAccountKey $automationAccountKey
+```
+
+## Onboard machines using the CustomScriptExtension
+```powershell
+#Install a new hybrid worker on a VM and register it to the target Azure Automazion Account
+$hybridWorkerGroupName = "myGroup"
+$workspaceId = "62b7bb25-7bfa-4006-bffc-a1c8667e37f6"
+$workspaceKey = "HxxiiaHH292+np3rrG03qIFGoqiJL1pdn/fHagV+VwFsqfQxstL5Pt+RuVM1goWyA9CbxZMiJChhF6IgKaphGQ=="
+$automationAccountEndpoint = "https://we-agentservice-prod-1.azure-automation.net/accounts/121d7ffa-30e3-41df-a028-d96a1c4f3f63"
+$automationAccountKey = "+OAZg2eFUSWPDssfKSQ/9pNoodShYP0yh3MQG7m83zbGUG5VjW5pjIfZBndYNfBNLsndiHYQLlyjkrmhgyE8eg="
+
+./hybridworker.ps1 -HybridGroupName $hybridWorkerGroupName -WorkspaceId $workspaceId -WorkspaceKey $workspaceKey -AutomationAccountEndpoint $automationAccountKey -AutomationAccountKey $automationAccountKey
+```
---
+++ hybridworker.ps1
@@ -0,0 +1,97 @@
+# EXAMPLE ./hybridworker.ps1 -WorkspaceId "xxxxxxxxx" -WorkspaceKey "xxxxxxxxx" -AutomationAccountKey "xxxxxxxx" -AutomationAccountEndpoint "xxxxxxx"
+#Requires -RunAsAdministrator
+
+Param (
+# OMS Workspace
+[Parameter(Mandatory=$false)]
+[String] $WorkspaceId,
+
+# OMS Workspace Key
+[Parameter(Mandatory=$false)]
+[String] $WorkspaceKey,
+
+# Automation Account Endpoint
+[Parameter(Mandatory=$true)]
+[String] $AutomationAccountEndpoint ,
+
+# Automation Account Key
+[Parameter(Mandatory=$true)]
+[String] $AutomationAccountKey ,
+
+# Hyprid Group
+[Parameter(Mandatory=$true)]
+[String] $HybridGroupName = "windows"
+)
+
+# Check for the MMA on the machine
+try {
+
+    $mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
+    
+    Write-Output "Configuring the MMA..."
+    $mma.AddCloudWorkspace($WorkspaceId, $WorkspaceKey)
+    $mma.ReloadConfiguration()
+
+} catch {
+    # Download the Microsoft monitoring agent
+    Write-Output "Downloading and installing the Microsoft Monitoring Agent..."
+
+    # Check whether or not to download the 64-bit executable or the 32-bit executable
+    if ([Environment]::Is64BitProcess) {
+        $Source = "http://download.microsoft.com/download/1/5/E/15E274B9-F9E2-42AE-86EC-AC988F7631A0/MMASetup-AMD64.exe"
+    } else {
+        $Source = "http://download.microsoft.com/download/1/5/E/15E274B9-F9E2-42AE-86EC-AC988F7631A0/MMASetup-i386.exe"
+    }
+
+    $Destination = "$env:temp\MMASetup.exe"
+
+    $null = Invoke-WebRequest -uri $Source -OutFile $Destination
+    $null = Unblock-File $Destination
+
+    # Change directory to location of the downloaded MMA
+    cd $env:temp
+
+    # Install the MMA
+    $Command = "/C:setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=$WorkspaceID" + " OPINSIGHTS_WORKSPACE_KEY=$WorkspaceKey " + " AcceptEndUserLicenseAgreement=1"
+    .\MMASetup.exe $Command
+
+}
+
+# Sleep until the MMA object has been registered
+Write-Output "Waiting for agent registration to complete..."
+
+# Timeout = 180 seconds = 3 minutes
+$i = 18
+
+do {
+    
+    # Check for the MMA folders
+    try {
+        # Change the directory to the location of the hybrid registration module
+        cd "$env:ProgramFiles\Microsoft Monitoring Agent\Agent\AzureAutomation"
+        $version = (ls | Sort-Object LastWriteTime -Descending | Select -First 1).Name
+        cd "$version\HybridRegistration"
+
+        # Import the module
+        Import-Module (Resolve-Path('HybridRegistration.psd1'))
+
+        # Mark the flag as true
+        $hybrid = $true
+    } catch{
+
+        $hybrid = $false
+
+    }
+    # Sleep for 10 seconds
+    Start-Sleep -s 10
+    $i--
+
+} until ($hybrid -or ($i -le 0))
+
+if ($i -le 0) {
+    throw "The HybridRegistration module was not found. Please ensure the Microsoft Monitoring Agent was correctly installed."
+}
+
+# Register the hybrid runbook worker
+Write-Output "Registering the hybrid runbook worker..."
+Add-HybridRunbookWorker -Name $HybridGroupName -EndPoint $AutomationAccountEndpoint -Token $AutomationAccountKey
